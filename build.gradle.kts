a55acc900d03b59eeb145c7ab15991045c0a1f0c
Ing Zhen Lee <ingzhen2003@gmail.com>
2026-05-09 17:29:03 +0800

    Add Room and KSP dependencies for Analytics

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     id("org.jetbrains.kotlin.multiplatform") version "2.2.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21" apply false
+    id("com.google.devtools.ksp") version "2.2.21-2.0.4" apply false
 }
